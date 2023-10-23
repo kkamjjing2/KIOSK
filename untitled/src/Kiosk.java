ca6ac47b0d35aabde9158f39944cbb87416b5a9b
kkamjjing2 <kkamjjing2@gmail.com>
2023-10-23 15:23:30 +0900

    [UPDATE] - (Kiosk Class) menus 배열 추가

--- untitled/src/Kiosk.java
+++ untitled/src/Kiosk.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Kiosk {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Order order = new Order();
+
+        Menu[] menus = {
+                new Menu("Burgers", "앵거스 비프 통살을 다져만든 버거"),
+                new Menu("Frozen Custard", "매장에서 신선하게 만드는 아이스크림"),
+                new Menu("Drinks", "매장에서 직접 만드는 음료"),
+                new Menu("Beer", "뉴욕 브루클린 브루어리에서 양조한 맥주")
+        };
     }
 }

ab0e81a276607757c029917ca426399ef370511a
kkamjjing2 <kkamjjing2@gmail.com>
2023-10-23 15:17:45 +0900

    [UPDATE] - (Order Class) 메서드 3개 추가

--- untitled/src/Order.java
+++ untitled/src/Order.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Order {
+    ArrayList<Product> cart = new ArrayList<>();
+
+    public void addToCart(Product product) { // 장바구니에 상품 추가하는 메서드(ArrayList.add(E e))
+        cart.add(product);
+    }
+
+    public void clearCart() {               // 장바구니의 모든 상품을 삭제하는 메서드(ArrayList.clear())
+        cart.clear();
+    }
+
+    public double calculateTotal() {        // 장바구니 상품 가격 합계를 산출하는 메서드
+        double total = 0;
+        for (Product product : cart) {
+            total += product.price;
+        }
+        return total;
+    }
 }
